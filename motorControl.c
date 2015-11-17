74e6a990c59b7045842e78bc36fba962fffe9daf
Jacob Adams <tookmund@gmail.com>
2015-11-17 15:44:18 -0500

    Remove unecessary include stuff

--- motorControl.c
+++ motorControl.c
@@ -1,7 +1,3 @@
-#pragma config(StandardModel, "RVW CLAWBOT")
-#include "motorControl.h"
-// motor is a reserved word so mtr instead
-
 void runMotor(int mtr, int speed, float seconds)
 {
 	writeDebugStreamLine("Motor %d at %d speed for %g seconds",mtr,speed,seconds);
