8fe13d1b9c3f239d40df8af0a53aa16eaa9ed5c9
Jacob Adams <tookmund@gmail.com>
2015-12-08 16:39:32 -0500

    rotate: add start and stop for clockwise rotation

--- rotate.c
+++ rotate.c
@@ -2,6 +2,16 @@
 #define RF 1
 #define RB 2
 
+void startRotateClock(int speed)
+{
+	motor[RF] = speed;
+	motor[RB] = speed;
+}
+void stopRotateClock()
+{
+	motor[RF] = 0;
+	motor[RB] = 0;
+}
 void rotateClock(int speed, float sec)
 {
 	run2IndMotors(RF,RB,speed,speed,sec);
