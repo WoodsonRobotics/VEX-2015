fcd464d8fc8870b782bc59f4bb506a6b64837471
Jacob Adams <tookmund@gmail.com>
2016-01-02 16:36:49 -0500

    main: merge main and competition code

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#pragma platform(VEX)
+
+#pragma competitionControl(Competition)
+// Timing for Competitions
+#pragma autonomousDuration(20)
+#pragma userControlDuration(120)
+
+// Uncomment for competitions
+//#include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
+
 // Hacky dependency control
 // Expect this to break at some point
 #include "motorControl.c"
@@ -10,5 +20,18 @@
 task main()
 {
 	startLaunch();
+	//joystickControl();
 	while(1){}
+}
+
+void pre_auton() {}
+
+task autonomous()
+{
+	startLaunch();
+	while(1){}
+}
+task usercontrol()
+{
+	joystickControl();
 }
