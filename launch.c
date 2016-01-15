54ff31620d86228e222c48af3b7776545710a043
Jacob Adams <tookmund@gmail.com>
2016-01-15 18:14:17 -0500

    launch: Add flicker

--- launch.c
+++ launch.c
@@ -1,6 +1,10 @@
 // Launcher Motors
 #define LL 3
 #define LR 4
+// Flick Motor
+#define FM 5
+// Number of Balls
+#define NB 3
 #define LSPD 70
 
 void startLaunch()
@@ -12,4 +16,18 @@ void stopLaunch()
 {
 	motor[LL] = 0;
 	motor[LR] = 0;
+}
+void flick()
+{
+	// Flick the projectiles into the launcher
+	runMotor(FM,LSPD,0.1);
+	runMotor(FM,-LSPD,0.1);
+}
+void launch()
+{
+	for(int i = 0; i < 3;i++)
+	{
+		flick();
+		delay(1000);
+	}
 }
