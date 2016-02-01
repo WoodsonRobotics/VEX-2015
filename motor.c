27ff71becf3a2b75f389203a2dbecffcaed672f9
Jacob Adams <tookmund@gmail.com>
2016-02-01 15:47:49 -0500

    motor: Put all motor defines in one file

---
+++ motor.c
@@ -0,0 +1,19 @@
+// Motors
+
+// Right
+#define RF 1
+#define RB 2
+
+// Left
+#define LF 3
+#define LB 4
+
+// Feeder
+#define FD 5
+
+// Lifter
+#define LT 6
+
+// Launcher
+#define LR 7
+#define LL 8
