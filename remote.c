3c020b34f9f9a6ca389b3f9963e95307a04e7c06
Jacob Adams <tookmund@gmail.com>
2016-02-02 15:51:54 -0500

    remote: Add button controls and simplify function naming

--- remote.c
+++ remote.c
@@ -1,17 +1,24 @@
-bool btnPress(TVexJoysticks bt)
+bool btn(TVexJoysticks bt)
 {
 	if(vexRT[bt] == 1) return true;
 	else return false;
 }
 void joystickControl()
 {
-	// Genius code by Daniel
-	// Blame him when it breaks
+
 	int v, v2, h, h2;
 	float a, b;
 
 	while(true)
 	{
+		if (btn(Btn5U)) startLaunch();
+		if (btn(Btn5D)) stopLaunch();
+		if (btn(Btn6U)) startFeeder();
+		if (btn(Btn6D)) stopFeeder();
+		if (btn(Btn8R)) flick();
+
+		// Genius code by Daniel
+		// Blame him when it breaks
 		v = motor[Ch4];
 		h = motor[Ch3];
 
