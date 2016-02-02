f6ceed7fac06917fbdc2e20c754bcb24e87f2f98
Jacob Adams <tookmund@gmail.com>
2016-02-02 15:52:35 -0500

    main: Fix dependencies and and add competition functionality

--- main.c
+++ main.c
@@ -14,22 +14,27 @@
 // Expect this to break at some point
 #include "motor.c"
 #include "move.c"
-#include "remote.c"
 #include "launch.c"
 #include "intake.c"
+#include "remote.c"
 
 void pre_auton()
 {}
 
 task autonomous()
 {
-
+	startLaunch();
+	flickPrel();
+	startIntake();
+	move(100);
+	delay(1000);
+	move(0);
 }
 task usercontrol()
 {
-
+	startIntake();
 	joystickControl();
 }
 
-//task main() { startTask(autonomous); }
+task main() { startTask(autonomous); }
 //task main() { startTask(usercontrol); }
